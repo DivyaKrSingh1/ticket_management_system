6537416fc99137b0077199eaaa9a67de7155931a
Divya Kumar Singh <dksingh8957@gmail.com>
2026-05-10 13:00:22 +0530

    Update api.js

--- Ticket Management System/src/services/api.js
+++ Ticket Management System/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3030/api'
+    baseURL: 'https://ticket-management-system-1-8q08.onrender.com'
 });
 
 API.interceptors.request.use((config) => {
