5d1f2bb512ec42accc2237e3a1d125098a9a0617
Divya Kumar Singh <dksingh8957@gmail.com>
2026-05-12 17:51:01 +0530

    fixed image upload

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,35 +1,66 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+
+// Create uploads folder automatically
+const uploadPath = 'uploads';
+
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath);
+}
+
 
 const storage = multer.diskStorage({
+
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+
+        cb(null, uploadPath);
     },
 
     filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
+
+        cb(
+            null,
+            Date.now() + path.extname(file.originalname)
+        );
     }
 });
 
+
 const fileFilter = (req, file, cb) => {
-    const allowedTypes = /jpeg|jpg|png|webp/;
 
-    const extname = allowedTypes.test(
-        path.extname(file.originalname).toLowerCase()
-    );
+    const allowedTypes =
+        /jpeg|jpg|png|webp/;
 
-    const mimetype = allowedTypes.test(file.mimetype);
+    const extname =
+        allowedTypes.test(
+            path.extname(file.originalname).toLowerCase()
+        );
+
+    const mimetype =
+        allowedTypes.test(file.mimetype);
 
     if (extname && mimetype) {
+
         return cb(null, true);
+
     } else {
-        cb(new Error('Only images are allowed'));
+
+        cb(new Error('Only image files are allowed'));
     }
 };
 
+
 const upload = multer({
+
     storage,
-    fileFilter
+
+    fileFilter,
+
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
 });
 
 module.exports = upload;
